37ee44b70d688ebbe0fd66186132bae490ad4328
Vidyavantam <vidyavantam@gmail.com>
2022-12-17 16:37:00 +0530

    hi

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/wallisconsultancy',
+  assetPrefix: '/wallisconsultancy/',
 }
 
 module.exports = nextConfig
