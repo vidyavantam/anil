ef7c42880860b557fc90977ab96d8ea806e8ec64
Vidyavantam <vidyavantam@gmail.com>
2022-12-17 18:51:46 +0530

    h

--- pages/components/com.js
+++ pages/components/com.js
@@ -50,7 +50,7 @@ export function AdhyayanID() {
           <div className="lg:col-30" style={{ margin: '20px 0 20px 0' }}>
             <div className={styles.card} style={{ maxWidth: "100%", height: "max-content" }}>
               <div style={{ margin: "10px 0" }}>
-                <img src={`/${url}/yajna.png`} style={{ width: "95%", height: "95%" }} />
+                <img src={`/${url}/book.svg`} style={{ width: "95%", height: "95%" }} />
               </div>
               <h2>{da[0].name}</h2>
             </div>
